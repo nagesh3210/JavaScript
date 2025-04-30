db8d2e71fe2b219775e290751f129fdaf7087349
nagesh3210 <nmathapati088@gmail.com>
2025-04-30 16:06:42 +0000

    2nd commit

---
+++ basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId =2000
+let accountEmail = "hiteshGmail.com"
+var accountpassword = "12345"
+accountCity = "jaipur"
+
+let accounstate;
+
+//accountId= 2
+
+
+console.log(accountId);
+
+console.table([accountEmail,accountpassword,accountCity,accounstate])
---
+++ basics/02_datatypes.js
@@ -0,0 +1,8 @@
+
+console.log(typeof 5)
+console.log(typeof undefined)
+
+console.log(typeof null)
+
+
+
