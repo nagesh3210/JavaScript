7d45b450c9334a4fe36634f3e3922e5691076ec9
nagesh3210 <nmathapati088@gmail.com>
2025-05-01 16:35:18 +0000

    3rd commit

--- basics/02_datatypes.js
+++ basics/02_datatypes.js
@@ -4,5 +4,28 @@ console.log(typeof undefined)
 
 console.log(typeof null)
 
+console.log(fun)
 
+
+var fun =function ()
+{
+    cpnsole.log("Hello from funcgtion");
+}
+
+
+var a=20;
+ var b= String(a)
+
+console.log(typeof b)
+
+
+
+let num1, num2 ,num3
+
+num1 = num2 = num3 = 2+2 ;
+
+
+console.log(num1)
+console.log(num2)
+console.log(num3)
 
