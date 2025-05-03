da675b1e200676f0cf5321bd9a5a29757f00c308
nagesh3210 <nmathapati088@gmail.com>
2025-05-03 07:18:20 +0000

    date file push

--- basics/03_comparisions.js
+++ basics/03_comparisions.js
@@ -9,3 +9,29 @@ console.log(null >= 0);
 console.log("2" > 3);
 
 
+const hero = ["1","2"]
+
+const hii = Symbol(45)
+
+console.log(typeof hii);
+console.log(typeof hero);
+
+
+const num = "hello";
+
+console.log(num.length)
+
+num1=num;
+console.log(num1)
+
+
+ num1 =10;
+
+
+console.log(num1)
+
+console.log(num.toUpperCase())
+console.log(num)
+
+
+console .log(`hello my name is ${num} chaudhary`)
---
+++ basics/date.js
@@ -0,0 +1,13 @@
+
+let myDAte =new Date()
+
+console.log(myDAte);
+console.log(myDAte.toDateString());
+console.log(myDAte.toLocaleString());
+console.log(myDAte.toJSON());
+console.log(myDAte.toISOString());
+
+
+let
+
+
